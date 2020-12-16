45c963aef89376217b7d960af943232b86aca334
DerEros <DerEros@users.noreply.github.com>
2020-12-16 21:25:20 +0100

    Create basic window via SDL

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,13 @@
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().expect("Could not initialize SDL2");
+    let video_subsystem = sdl_context.video().expect("Could not access video subsystem in SDL2");
+
+    let _window = video_subsystem.window("Mirror UI",  768, 1024)
+        .position_centered()
+        .build()
+        .expect("Could not create window");
+
+    ::std::thread::sleep(Duration::new(10, 0));
 }
